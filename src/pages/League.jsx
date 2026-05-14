2bab95fd01114d6358cbc6f4613d11914d18612b
Santiago <s.urbinazeballos@gmail.com>
2026-05-14 11:29:02 -0300

    fix league mayuscula

--- src/pages/League.jsx
+++ src/pages/League.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import TeamLeagueTable from "../components/teamLeagueTable";
+import TeamLeagueTable from "../components/%eamLeagueTable";
 import TopBoxLeague from "../components/TopBoxLeague";
 
 const League = () => {
