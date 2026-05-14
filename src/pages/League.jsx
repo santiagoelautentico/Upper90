6670416860673bb4779c429e2db6fd47f02ebf3c
Santiago <s.urbinazeballos@gmail.com>
2026-05-14 11:31:03 -0300

    fix Team mayuscula

--- src/pages/League.jsx
+++ src/pages/League.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import TeamLeagueTable from "../components/%eamLeagueTable";
+import TeamLeagueTable from "../components/TeamLeagueTable";
 import TopBoxLeague from "../components/TopBoxLeague";
 
 const League = () => {
